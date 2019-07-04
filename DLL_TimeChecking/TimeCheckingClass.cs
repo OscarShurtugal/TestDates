242c21c9f334c039ece20b03eae51350268476df
Oscar Shurtugal <oass1_power@hotmail.com>
2019-07-04 16:17:05 -0500

    First Commit. DLL tested and ready for production environment

--- DLL_TimeChecking/TimeCheckingClass.cs
+++ DLL_TimeChecking/TimeCheckingClass.cs
@@ -4,52 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestDates
+namespace DLL_TimeChecking
 {
-    class Program
+    public class TimeCheckingClass
     {
-        static void Main(string[] args)
-        {
-            DateTime lowerLimitSpan = DateTime.Parse("22:30");
-            DateTime upperLimitSpan = DateTime.Parse("5:30");
-            string result = checkTimeSpan(lowerLimitSpan,upperLimitSpan);
-            //Console.WriteLine(checkTimeSpan(lowerLimitSpan, upperLimitSpan));
-
-            Console.WriteLine(checkTimeSpan("",""));
-
-            Console.ReadLine();
-        }
-
-        /// <summary>
-        /// This Method will check if the actual time is between a defined Time Span
-        /// </summary>
-        /// <param name="lowerLimitSpan"></param>
-        /// <param name="upperLimitSpan"></param>
-        /// <returns>A string with the message if its within or out the TimeSpan</returns>
-        private static string checkTimeSpan(DateTime lowerLimitSpan, DateTime upperLimitSpan)
-        {
-
-
-            DateTime target = DateTime.Now;
-
-
-
-            while (lowerLimitSpan.ToShortTimeString()!=upperLimitSpan.ToShortTimeString())
-            {
-                if (lowerLimitSpan.ToShortTimeString()==target.ToShortTimeString())
-                {
-                    return "The Target is within the TimeSpan";
-
-                }
-
-                lowerLimitSpan=lowerLimitSpan.AddMinutes(1);
-            }
-
-            return "The target is out of the TimeSpan";
-
-
-
-        }
 
         /// <summary>
         /// This Method will check if the actual time is between a defined Time Span.
@@ -60,14 +18,14 @@ namespace TestDates
         /// <param name="lowerLimitSpan"></param>
         /// <param name="upperLimitSpan"></param>
         /// <returns>A string with the message if its within or out the TimeSpan</returns>
-        private static string checkTimeSpan(string lowerLimit, string upperLimit)
+        public string checkTimeSpan(string lowerLimit, string upperLimit)
         {
-            
+
             DateTime upperLimitSpan;
 
             DateTime lowerLimitSpan;
 
-            if (lowerLimit is null || lowerLimit == "")
+            if (lowerLimit is null||lowerLimit=="")
                 lowerLimitSpan=DateTime.Parse("0:00");
 
             else
@@ -75,7 +33,7 @@ namespace TestDates
                 try
                 {
 
-                    lowerLimitSpan = DateTime.Parse(lowerLimit);
+                    lowerLimitSpan=DateTime.Parse(lowerLimit);
                 }
                 catch (Exception e)
                 {
@@ -84,7 +42,7 @@ namespace TestDates
 
             }
 
-            if (upperLimit is null || upperLimit == "")
+            if (upperLimit is null||upperLimit=="")
                 upperLimitSpan=DateTime.Parse("23:59");
             else
             {
@@ -119,5 +77,6 @@ namespace TestDates
 
 
         }
+
     }
 }
